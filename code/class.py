82d557853cc455b9a66e4891307f7bdaa3920653
lylinsh <lylin_sh@163.com>
2019-10-07 20:32:35 +0800

    Update class.py

--- code/class.py
+++ code/class.py
@@ -34,36 +34,37 @@ class C(A, B):
         print(self.n) 
 
 
+class D(object):
+    n0 = 1
+    __n1 = 0
+    def __init__(self, n):
+        self.n2 = n
+
+    def setN0(self, n):
+        D.n0 = n
+    
+    def setN1(self, n):
+        D.__n1 = n 
+    
+    def getN1(self):
+        return D.__n1
+
+
 if __name__ == "__main__":
     c1 = C(4)
     c1.setN(5)
 
+    d1 = D(2) 
+    d2 = D(3)
+    print("the n0 of class1 and class2 are: {}, {}".format(d1.n0, d2.n0))
+    print("the n2 of class1 and class2 are: {}, {}".format(d1.n2, d2.n2))
+    try:
+        print(d1._n1)
+    except:
+        print("Can not get the private value directly!")
+    d1.setN1(4)
+    print(d1._D__n1)
+    n3 = d2.getN1()
+    print(n3)  
 
-    # d1 = D(2) 
-    # d2 = D(3)
-    # print("the n0 of class1 and class2 are: {}, {}".format(d1.n0, d2.n0))
-    # print("the n2 of class1 and class2 are: {}, {}".format(d1.n2, d2.n2))
-    # try:
-    #     print(d1._n1)
-    # except:
-    #     print("Can not get the private value directly!")
-    # d1.setN1(4)
-    # print(d1._D__n1)
-    # n3 = d2.getN1()
-    # print(n3)  
 
-
-# class D(object):
-#     n0 = 1
-#     __n1 = 0
-#     def __init__(self, n):
-#         self.n2 = n
-
-#     def setN0(self, n):
-#         D.n0 = n
-    
-#     def setN1(self, n):
-#         D.__n1 = n 
-    
-#     def getN1(self):
-#         return D.__n1
